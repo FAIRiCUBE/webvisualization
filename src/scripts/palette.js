7b25a59722d04248e1d26b886b774bc322f65528
Dan Moran MBA15 <dmoran@alumni.whitman.edu>
2024-11-23 17:44:13 +0100

    lots

--- src/scripts/palette.js
+++ src/scripts/palette.js
@@ -3,55 +3,58 @@ import * as d3 from 'd3';
 
 
 
-export function getInterpolateBand1AsColor(pal) {
+export function getInterpolateBand1AsColor() {
 
-    // If pal is not specified, use 'rgb' if palette-rgb is checked, otherwise use 'A'
-    if (!pal) {
-        if (document.getElementById('palette-rgb').checked) pal='rgb';
-        else pal = document.querySelector('input[name="palette"]:checked').value;
-    }
+  const pal = localStorage.getItem('palette') || 'd3.interpolateBlues';
 
-    // Hide #colorbar element
-    document.getElementById('colorbar').style.display = 'none';
-
-    if (pal.toLowerCase()=='rgb')return { color: ['array', ['band', 1], ['band', 2], ['band', 3], 1] };
-
-    document.getElementById('colorbar').style.display = 'block';
-
-    let color = null;
-    if (pal=='A') color = d3.scaleSequential(d3.interpolateRainbow);
-    if (pal=='B') color = d3.scaleSequential(d3.interpolateViridis);
-    if (pal=='C') color = d3.scaleSequential(d3.interpolateWarm);
-    if (pal=='D') color = d3.scaleSequential(d3.interpolateGnBu);
+  if (pal.toLowerCase()=='rgb')
+    return { color: ['array', ['band', 1], ['band', 2], ['band', 3], 1] };
 
 
-      const clr_arr = [
-        'interpolate',
-        ['linear'],
-        ['band', 1],
-        0,
-        [255,0,0,0],
-        0.001,
-        color(0.001),
-        0.1,
-        color(0.1),
-        0.1,
-        color(0.1),
-        0.2,
-        color(0.2),
-        0.4,
-        color(0.4),
-        0.6,
-        color(0.6),
-        1,
-        color(1)
-      ];
-      interpolation_to_colorbar(clr_arr.slice(3));
-      return {color: clr_arr};
+  let color = getPaletteAsFunction();
+  // If inverted, change the color function domain to 1,0
+  if (localStorage.getItem('invertpalette')==='true') color.domain([1,0]);
 
+
+  const clr_arr = [
+    'interpolate',
+    ['linear'],
+    ['band', 1],
+    0,
+    [255,0,0,0],
+    0.001,
+    color(0.001),
+    0.1,
+    color(0.1),
+    0.1,
+    color(0.1),
+    0.2,
+    color(0.2),
+    0.4,
+    color(0.4),
+    0.6,
+    color(0.6),
+    1,
+    color(1)
+  ];
+  return {color: clr_arr};
     
 }
 
+
+export function getPaletteAsGradient() {
+
+  const color = getPaletteAsFunction();
+  if (!color) return 'red';
+  const gradient = [];
+  for(let i=0; i<1; i+=0.1) {
+    const c = color(i);
+    gradient.push(c);
+  }
+  const invert = localStorage.getItem('invertpalette')==='true';
+  if (invert) gradient.reverse();
+  return `linear-gradient(to right, ${gradient.join(',')})`;
+}
 
 // Given a MapLibre color interpolation array like [value, color, value, color, ...], create a
 // colorbar elelement with a CSS background gradient 
@@ -79,3 +82,24 @@ function interpolation_to_colorbar(interp){
 
 }
 
+
+
+
+
+
+export function getPaletteAsFunction() {
+
+  const palette = localStorage.getItem('palette') || 'd3.interpolateBlues';
+  if (palette=='rgb') return null;
+  const paletteFunction = eval(palette);
+
+  const invert = localStorage.getItem('invertpalette');
+
+  /*let min = localStorage.getItem('min') ?? 0;
+  let max = localStorage.getItem('max') ?? 1;
+  if (invert==='true') {
+    [min, max] = [max, min];
+  }
+*/
+  return d3.scaleSequential(paletteFunction);//.domain([min, max]);
+}
