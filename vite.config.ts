ac6d87b0d5c72f3e8c99c457992c6db6a45df1e4
morandd <dmoran@alumni.whitman.edu>
2025-11-20 14:38:42 +0100

    significant version bump to Astro v5, with associated Vite adjustments, responding to XSS error discovered Astro v4.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
+
+
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(),
+    nodePolyfills({
+      // To exclude specific polyfills, add them to this list.
+      exclude: [
+        'fs', // Excludes the polyfill for `fs` and `node:fs`.
+      ],
+      // Whether to polyfill specific globals.
+      globals: {
+        Buffer: true, // can also be 'build', 'dev', or false
+        global: true,
+        process: true,
+      },
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    })
+
+  ],
   optimizeDeps: {
     include: ['node_modules/worker_threads'],
   },
